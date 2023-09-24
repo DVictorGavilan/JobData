2ba9db5b7a0eee1a60dac0a91535e888fa76629b
Daniel Gavilán <danigavipedro96@gmail.com>
2023-09-24 19:27:34 +0200

    Ejecucion 2023-09-24

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from datetime import date
-from math import ceil
-
 import pandas as pd
 import requests
+
+from math import ceil
 from bs4 import BeautifulSoup
-from bs4.element import Tag, ResultSet
 from pandas import DataFrame
 from requests import Response
+from bs4.element import Tag, ResultSet
 
 
 def get_html(url: str) -> BeautifulSoup:
@@ -19,10 +19,10 @@ def get_html(url: str) -> BeautifulSoup:
 
 
 def gen_d_provincias_info() -> dict[str: list[str]]:
-    d_provincias_info: dict[str: list[str]] = {
-        'id_provincia': list(),
-        'provincia': list(),
-        'num_ofertas': list()
+    d_provincias_info = {
+        'id_provincia': [],
+        'provincia': [],
+        'num_ofertas': []
     }
     return d_provincias_info
 
@@ -35,19 +35,20 @@ def get_provincias_info() -> dict[str: list[str]]:
         header: str = soup.find('h1', class_='h5 h6-xs').text
         header_info: list[str] = [info.strip() for info in header.split(' ')]
         d_provincias_info['id_provincia'].append(id_provincia)
-        d_provincias_info['provincia'].append(' '.join(header_info[6:-4]))
-        d_provincias_info['num_ofertas'].append(header_info[0].replace('.', ''))
+        d_provincias_info['provincia'].append(' '.join(header_info[6:]))
+        d_provincias_info['num_ofertas'].append(
+            header_info[0].replace('.', ''))
     return d_provincias_info
 
 
 def gen_d_empleos_info() -> dict[str: list[str]]:
     dic_empleos_info: dict[str: list[str]] = {
-        'nombre_empleo': list(),
-        'empresa': list(),
-        'provincia': list(),
-        'tecnologias': list(),
-        'descripcion': list(),
-        'url': list()
+        'nombre_empleo': [],
+        'empresa': [],
+        'provincia': [],
+        'tecnologias': [],
+        'descripcion': [],
+        'url': []
     }
     return dic_empleos_info
 
@@ -60,16 +61,20 @@ def gen_empleos_info(df: DataFrame):
         for page in gen_num_pages(num_pages):
             url = f'https://www.tecnoempleo.com/busqueda-empleo.php?pr=,{id_provincia},&pagina={page}'
             soup: BeautifulSoup = get_html(url)
-            divs_info: ResultSet[Tag] = soup.find_all('div', class_='p-2 border-bottom py-3 bg-white')
+            divs_info: ResultSet[Tag] = soup.find_all(
+                'div', class_='p-2 border-bottom py-3 bg-white')
             for div in divs_info:
-                empleo: str = div.find('h3', class_='h5 h6-xs mb-2').text.strip()
-                empleo_url: str = div.find('a', class_='font-weight-bold text-gray-700 link-muted')['href']
-                empresa: str = div.find('a', class_='text-primary').text.strip()
+                empleo: str = div.find(
+                    'h3', class_='h5 h6-xs mb-2').text.strip()
+                empleo_url: str = div.find(
+                    'a', class_='font-weight-bold text-gray-700')['href']
+                empresa: str = div.find(
+                    'a', class_='text-primary').text.strip()
                 provincia: str = df_provincias_info[provincia_num]['provincia'].values[0]
                 lenguajes: str = ' - '.join([leng.text.strip() for leng in div.find_all('a',
-                                                                                        class_='badge-pill badge-soft badge-primary text-primary text-warning-hover py-1 px-2 fs--13 mr-1')])
+                                                                                        class_='badge bg-primary-soft bg-primary-hover fw-normal font-weight-normal fs--14 mr-2 me-2')])
                 descripcion: str = ' '.join([desc.strip() for desc in div.find('div',
-                                                                               'col-12 col-lg-3 text-gray-600 pt-2 line-height-10 text-right hidden-md-down').text.split()])
+                                                                               class_='col-12 col-lg-3 text-gray-600 pt-2 line-height-10 text-right hidden-md-down').text.split()])
                 d_empleos_info['nombre_empleo'].append(empleo)
                 d_empleos_info['empresa'].append(empresa)
                 d_empleos_info['provincia'].append(provincia)
@@ -88,7 +93,8 @@ df_provincias_info: DataFrame = DataFrame(get_provincias_info())
 path_provincias_info: str = f'data/provincias_info_{date.today()}.csv'
 df_provincias_info.to_csv(path_provincias_info, index=False, sep=';')
 
-df_provincias_info: DataFrame = pd.read_csv(path_provincias_info, header=[0], sep=';')
+df_provincias_info: DataFrame = pd.read_csv(
+    path_provincias_info, header=[0], sep=';')
 
 df_empleos_info: DataFrame = DataFrame(gen_empleos_info(df_provincias_info))
 path_provincias_info: str = f'data/empleos_info_{date.today()}.csv'
