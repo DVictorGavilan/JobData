7568bcad47d056b1b4f836d5ceb7b9317e56adca
unknown <dre21mayo@hotmail.com>
2023-09-30 12:40:14 -0500

    Create function add_list_technologies

--- job_data/transformation.py
+++ job_data/transformation.py
@@ -1,6 +1,6 @@
 import re
 import pandas as pd
-
+from typing import List
 
 def add_date(value: str) -> str:
     value = value.strip()
@@ -12,8 +12,10 @@ def add_separete_description(value: str) -> str:
     return re.sub(r'(?<=[a-z])(?=[A-Z])', ' ', value)
 
 
-def add_list_technologies(value: str) -> list:
-    return []
+def add_list_technologies(value: str) -> List[str]:
+    # Dividir la cadena en una lista usando ' - ' como separador
+    tech_list = value.split(' - ')
+    return tech_list
 
 def normalized_technologies(technologies: list) -> list:
     return []
--- main_dante.py
+++ main_dante.py
@@ -8,7 +8,9 @@ data_job = pd.read_csv('data/empleos_info_2023-09-24.csv', sep=';')
 for column in data_job.columns:
     quality.check_nulls(data_job, columna=column)
 
-data_job['aux_date']        = data_job['descripcion'].apply(transformation.add_date)
-data_job['aux_descripcion'] = data_job['descripcion'].apply(transformation.add_separete_description)
+# data_job['aux_date']        = data_job['descripcion'].apply(transformation.add_date)
+# data_job['aux_descripcion'] = data_job['descripcion'].apply(transformation.add_separete_description)
+data_job['new_tecnologies'] = data_job['tecnologias'].apply(transformation.add_list_technologies)
 
-print(data_job[['aux_date', 'aux_descripcion']].head().to_string())
+# print(data_job[['aux_date', 'aux_descripcion']].head().to_string())
+print(data_job['new_tecnologies'])
