585383e716518794e1fc99a21672996c968410c5
unknown <dre21mayo@hotmail.com>
2023-10-04 17:04:51 -0500

    Create function extract_salary_amount

--- job_data/transformation.py
+++ job_data/transformation.py
@@ -6,7 +6,7 @@ from typing import List
 DATE_FORMAT = '%d/%m/%Y'
 REGEX_SEPARETE_DESCPRIPTION = r'(?<=[a-z])(?=[A-Z])', ' '
 SEPARATE_TECHNOLOGIES = ' - '
-
+REGEX_EXTRACT_AMOUNT = r'(\d+(\.\d{3})*\s*€\s*-\s*\d+(\.\d{3})*\s*€)'
 
 def add_date(value: str) -> str:
     value = value.strip()
@@ -21,6 +21,12 @@ def add_separate_description(value: str) -> str:
 def add_list_technologies(value: str) -> List[str]:
     return value.split(SEPARATE_TECHNOLOGIES)
 
+def extract_salary_amount(value: str) -> str:
+    match = re.search(REGEX_EXTRACT_AMOUNT, value)
+    if match:
+        return match.group(1)
+    else:
+        return None
 
 def normalized_technologies(technologies: list) -> list:
     return []
--- main_dante.py
+++ main_dante.py
@@ -14,22 +14,23 @@ data_job = pd.read_csv('data/empleos_info_2023-09-30.csv', sep=';')
 
 # data_job['aux_date']        = data_job['descripcion'].apply(transformation.add_date)
 # data_job['aux_descripcion'] = data_job['descripcion'].apply(transformation.add_separate_description)
-data_job['new_tecnologies'] = data_job['tecnologias'].apply(
-    transformation.add_list_technologies)
-
+# data_job['new_tecnologies'] = data_job['tecnologias'].apply(
+#     transformation.add_list_technologies)
+data_job['aux_salary'] = data_job['descripcion'].apply(transformation.extract_salary_amount)
+print(data_job['aux_salary'].head(30))
 # print(data_job[['aux_date', 'aux_descripcion']].head().to_string())
-technologies = [technology.lower() for technologies in data_job['new_tecnologies'].to_list()
-                for technology in technologies]
+# technologies = [technology.lower() for technologies in data_job['new_tecnologies'].to_list()
+#                 for technology in technologies]
 # technologies = []
 
 # for technologies in data_job['new_tecnologies'].to_list():
 #   for technology in technologies:
 #      technologies.append(technology.lower())
 
-for tech, n in count_repeated_values(technologies).items():
-    if 'py' in tech:
-        print(tech, n)
+# for tech, n in count_repeated_values(technologies).items():
+#     if 'py' in tech:
+#         print(tech, n)
 
-print(len(technologies))
-print(len(set(technologies)))
-print(len(set([technology.lower() for technology in technologies])))
+# print(len(technologies))
+# print(len(set(technologies)))
+# print(len(set([technology.lower() for technology in technologies])))
