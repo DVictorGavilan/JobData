2ebbeb57ad0dd458260fed0404d7ea789b348f7d
DVictorGavilan <danigavipedro96@gmail.com>
2023-10-07 18:23:14 +0200

    Execution 2023-10-07 and clean tranformation

--- job_data/transformation.py
+++ job_data/transformation.py
@@ -2,31 +2,38 @@ import re
 import pandas as pd
 from typing import List
 
+from pandas import Timestamp
+
+if __name__ == '__main__':
+    print('sdf')
 
 DATE_FORMAT = '%d/%m/%Y'
-REGEX_SEPARETE_DESCPRIPTION = r'(?<=[a-z])(?=[A-Z])', ' '
+REGEX_SEPARATE_DESCRIPTION = r'(?<=[a-z])(?=[A-Z])', ' '
 SEPARATE_TECHNOLOGIES = ' - '
 REGEX_EXTRACT_AMOUNT = r'(\d+(\.\d{3})*\s*€\s*-\s*\d+(\.\d{3})*\s*€)'
 
-def add_date(value: str) -> str:
-    value = value.strip()
-    value = pd.to_datetime(value[:10], format=DATE_FORMAT)
+
+def add_date(raw_value: str) -> Timestamp:
+    raw_value = raw_value.strip()[:10]
+    value = pd.to_datetime(arg=raw_value, format=DATE_FORMAT)
     return value
 
 
 def add_separate_description(value: str) -> str:
-    return re.sub(REGEX_SEPARETE_DESCPRIPTION, value)
+    return re.sub(pattern=REGEX_SEPARATE_DESCRIPTION, string=value)
 
 
 def add_list_technologies(value: str) -> List[str]:
     return value.split(SEPARATE_TECHNOLOGIES)
 
-def extract_salary_amount(value: str) -> str:
+
+def extract_salary_amount(value: str) -> str | None:
     match = re.search(REGEX_EXTRACT_AMOUNT, value)
     if match:
         return match.group(1)
     else:
         return None
 
+
 def normalized_technologies(technologies: list) -> list:
-    return []
+    return technologies
