0be46d36aca1a4c6bc1a1a3ad14a5510a2582f36
unknown <dre21mayo@hotmail.com>
2023-10-14 17:59:28 -0500

    Final changes to functions and comments removal

--- job_data/transformation.py
+++ job_data/transformation.py
@@ -1,32 +1,20 @@
 import re
 import pandas as pd
 from typing import List
-from typing import Optional
 
 DATE_FORMAT = '%d/%m/%Y'
-REGEX_SEPARETE_DESCPRIPTION = r'(?<=[a-z])(?=[A-Z])', ' '
 SEPARATE_TECHNOLOGIES = ' - '
-# Captura sueldos o rangos de sueldos en formato €, con o sin separador de miles (puntos).
-# Ejemplos: "30.000€ - 42.000€", "30.000€"
 REGEX_EXTRACT_AMOUNT = r'(\d+(\.\d{3})*\€(?:\s*-\s*\d+(\.\d{3})*\€)?)|(\d+(\.\d{3})*\€)'
-# Expresión regular para encontrar las modalidades
-REGEX_MODALIDAD =  re.compile(r'(presencial|hibrido|híbrido|remoto)', re.IGNORECASE)
+REGEX_MODALIDAD = r'(presencial|hibrido|híbrido|remoto)'
 
 def add_date(value: str) -> str:
     value = value.strip()
     value = pd.to_datetime(value[:10], format=DATE_FORMAT)
     return value
 
-
-def add_separate_description(value: str) -> str:
-    return re.sub(REGEX_SEPARETE_DESCPRIPTION, value)
-
-
 def add_list_technologies(value: str) -> List[str]:
     return value.split(SEPARATE_TECHNOLOGIES)
 
-# La descripción debe seguir el formato de sueldo único (ejm. 30.000€) o rango de sueldos (ejm., 30.000€ - 42.000€).
-# Devuelve el sueldo o el rango de sueldos encontrado, o None si no se encontró ninguna información de sueldo.
 def extract_salary_amount(descripcion: str) -> str:
     matches = re.findall(REGEX_EXTRACT_AMOUNT, descripcion)
     if matches:
@@ -36,29 +24,28 @@ def extract_salary_amount(descripcion: str) -> str:
             return matches[0][3]
     return None
 
-def extract_salary_min(salary: Optional[str]) -> Optional[float]:
+def extract_salary_min(salary: str) -> float:
     if salary is None:
         return None
-    
+
     if '-' in salary:
         return float(salary.split(' - ')[0].replace('€', '').replace('.', ''))
     else:
         return float(salary.replace('€', '').replace('.', ''))
 
-def extract_salary_max(salary: Optional[str]) -> Optional[float]:
+def extract_salary_max(salary: str) -> float:
     if salary is None:
         return None
-    
+
     if '-' in salary:
         return float(salary.split(' - ')[1].replace('€', '').replace('.', ''))
     else:
         return float(salary.replace('€', '').replace('.', ''))
-    
-def extract_modalidad(descripcion: str) -> str:
-    matches = REGEX_MODALIDAD.findall(descripcion)
+
+def extract_modality(descripcion: str) -> str:
+    matches = re.findall(REGEX_MODALIDAD, descripcion)
     if matches:
-        # Devolver la primera modalidad encontrada
-        return matches[0].capitalize()  # Convertir la primera letra en mayúscula
+        return matches[0].capitalize()
     else:
         return None
 
--- main_dante.py
+++ main_dante.py
@@ -1,46 +1,25 @@
 import pandas as pd
-
 from job_data import quality, transformation
-
 
 def count_repeated_values(elements: list) -> dict:
     return {element: elements.count(element) for element in set(elements)}
 
-
 data_job = pd.read_csv('data/empleos_info_2023-09-30.csv', sep=';')
 
-# for column in data_job.columns:
-#    quality.check_nulls(data_job, columna=column)
+data_job['descripcion'] = data_job['descripcion'].str.lower()
 
-# data_job['aux_date']        = data_job['descripcion'].apply(transformation.add_date)
-# data_job['aux_descripcion'] = data_job['descripcion'].apply(transformation.add_separate_description)
-# data_job['new_tecnologies'] = data_job['tecnologias'].apply(
-#     transformation.add_list_technologies)
+data_job['aux_date']        = data_job['descripcion'].apply(transformation.add_date)
+print(data_job['aux_date'].head(15))
 
 '''Extraer los sueldos'''
 data_job['aux_salary'] = data_job['descripcion'].apply(transformation.extract_salary_amount)
 print(data_job['aux_salary'].head(10))
+
 '''Crear las nuevas columnas con las funciones aplicadas'''
 data_job['aux_salary_min'] = data_job['aux_salary'].apply(transformation.extract_salary_min)
 data_job['aux_salary_max'] = data_job['aux_salary'].apply(transformation.extract_salary_max)
 print(data_job[['aux_salary', 'aux_salary_min', 'aux_salary_max']].head(10))
+
 '''Extraer las modalidades de trabajo'''
-data_job['aux_modalidad'] = data_job['descripcion'].apply(transformation.extract_modalidad)
+data_job['aux_modalidad'] = data_job['descripcion'].apply(transformation.extract_modality)
 print(data_job['aux_modalidad'].head(25))
-
-# print(data_job[['aux_date', 'aux_descripcion']].head().to_string())
-# technologies = [technology.lower() for technologies in data_job['new_tecnologies'].to_list()
-#                 for technology in technologies]
-# technologies = []
-
-# for technologies in data_job['new_tecnologies'].to_list():
-#   for technology in technologies:
-#      technologies.append(technology.lower())
-
-# for tech, n in count_repeated_values(technologies).items():
-#     if 'py' in tech:
-#         print(tech, n)
-
-# print(len(technologies))
-# print(len(set(technologies)))
-# print(len(set([technology.lower() for technology in technologies])))
