3e538e0665ea86e57cf3e9eb69b2c348bfef8692
unknown <dre21mayo@hotmail.com>
2023-10-19 12:07:40 -0500

    Create quality is_empty and validate_date

--- job_data/quality.py
+++ job_data/quality.py
@@ -1,6 +1,19 @@
-from pandas import DataFrame
+import pandas as pd
+from pandas import DataFrame,Timestamp
 
 def check_nulls(df: DataFrame, columna: str) -> bool:
     if columna not in df.columns:
         raise ValueError(f'La columna "{columna}" no existe en el DataFrame.')
     return df[columna].isnull().any()
+
+#Comprobar si el df esta vacio
+def df_is_empty(df: DataFrame) -> bool:
+    return df.empty
+
+def validar_fecha(fecha: Timestamp) -> str:
+    if pd.isna(fecha):
+        return "Valor faltante (NaN)"
+    elif isinstance(fecha, Timestamp):
+        return "Cumple con el formato YYYY-MM-DD"
+    else:
+        return "No cumple con el formato requerido"
--- main_dante.py
+++ main_dante.py
@@ -6,20 +6,29 @@ def count_repeated_values(elements: list) -> dict:
 
 data_job = pd.read_csv('data/empleos_info_2023-09-30.csv', sep=';')
 
-data_job['descripcion'] = data_job['descripcion'].str.lower()
+# data_job['descripcion'] = data_job['descripcion'].str.lower()
 
 data_job['aux_date']        = data_job['descripcion'].apply(transformation.add_date)
-print(data_job['aux_date'].head(15))
+# print(data_job['aux_date'].head(15))
 
-'''Extraer los sueldos'''
-data_job['aux_salary'] = data_job['descripcion'].apply(transformation.extract_salary_amount)
-print(data_job['aux_salary'].head(10))
+# '''Extraer los sueldos'''
+# data_job['aux_salary'] = data_job['descripcion'].apply(transformation.extract_salary_amount)
+# print(data_job['aux_salary'].head(10))
 
-'''Crear las nuevas columnas con las funciones aplicadas'''
-data_job['aux_salary_min'] = data_job['aux_salary'].apply(transformation.extract_salary_min)
-data_job['aux_salary_max'] = data_job['aux_salary'].apply(transformation.extract_salary_max)
-print(data_job[['aux_salary', 'aux_salary_min', 'aux_salary_max']].head(10))
+# '''Crear las nuevas columnas con las funciones aplicadas'''
+# data_job['aux_salary_min'] = data_job['aux_salary'].apply(transformation.extract_salary_min)
+# data_job['aux_salary_max'] = data_job['aux_salary'].apply(transformation.extract_salary_max)
+# print(data_job[['aux_salary', 'aux_salary_min', 'aux_salary_max']].head(10))
 
-'''Extraer las modalidades de trabajo'''
-data_job['aux_modalidad'] = data_job['descripcion'].apply(transformation.extract_modality)
-print(data_job['aux_modalidad'].head(25))
+# '''Extraer las modalidades de trabajo'''
+# data_job['aux_modalidad'] = data_job['descripcion'].apply(transformation.extract_modality)
+# print(data_job['aux_modalidad'].head(25))
+
+'''Check is empty'''
+print(data_job.apply(quality.df_is_empty))
+'''Check dates'''
+# Validar 'c_fecha_nacimiento'
+print("Validación para 'aux_date':")
+# for fecha in data_job['aux_date']:
+#     print(f"{fecha} : {quality.validar_fecha(fecha)}")
+print(data_job['aux_date'].apply(quality.validar_fecha))
